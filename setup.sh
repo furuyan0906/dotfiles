4a14d30a8d8233c03d5f90887b92eaab0578075c
furuyan0906 <yanfuru96ongaku@gmail.com>
2023-03-21 17:58:30 +0900

    add setup script for Mac OSX

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 if [ "$(uname)" == "Linux" ]; then
     ./ubuntu/setup
+elif [ "$(uname)" == "Darwin" ]; then
+    ./macosx/setup
 else
     echo "Platform: $(uname -a)"
     echo "This platform is not supported."
