7c7b0d2e9603010cf5035ff58b5981a28e69bc82
furuyan0906 <yanfuru96ongaku@gmail.com>
2023-06-22 00:11:54 +0900

    update work in progress

--- nvim/lua/config/mason-lspconfig.lua
+++ nvim/lua/config/mason-lspconfig.lua
@@ -1,13 +1,20 @@
 ----------------------------------------------------------------------------------------------------------------------------
 -- williamboman/mason-lspconfig.nvim の設定
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 require('mason-lspconfig').setup()
-require('mason-lspconfig').setup_handlers {
+require('mason-lspconfig').setup_handlers({
     function (server_name)
-        require('lspconfig')[server_name].setup {
-            ---　補完プラグイン('cmp_nvim_lsp')をLSPと連携させる ---
-            capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lspconfig = require('lspconfig')
+        local cmp_nvim_lsp = require('cmp_nvim_lsp')
+
+        capabilities = cmp_nvim_lsp.default_capabilities()
+
+        ---　補完プラグイン('cmp_nvim_lsp')をLSPと連携させる ---
+        lspconfig[server_name].setup {
+            capabilities = capabilities
         }
     end,
-}
+})
 
