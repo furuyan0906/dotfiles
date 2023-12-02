d51e142018bf1186794f7c4ed8dde42333a697aa
furuyan0906 <yanfuru96ongaku@gmail.com>
2023-12-02 18:53:33 +0900

    remove fixing the tag of telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -150,7 +150,6 @@ return {
     },
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-treesitter/nvim-treesitter'
