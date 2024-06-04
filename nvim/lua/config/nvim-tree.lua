b5fdba9bff623ecd53bd4096917c42913ec5ad3c
furuyan0906 <yanfuru96ongaku@gmail.com>
2024-06-05 00:17:10 +0900

    update nvim-tree's configuration

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -3,6 +3,34 @@
 
 require('nvim-tree').setup({
     sort_by = 'case_sensitive',
+    view = {
+        adaptive_size = true,
+        float = {
+            enable = true,
+            quit_on_focus_loss = true,
+        },
+        width = {
+            min = 120,
+            max = -1,
+            padding = 0,
+        },
+    },
+    renderer = {
+        full_name = true,
+        icons = {
+            glyphs = {
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            },
+        }
+    },
     on_attach = function(bufnr)
         require('nvim-tree.api').config.mappings.default_on_attach(bufnr)
     end,
