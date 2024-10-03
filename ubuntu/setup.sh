1d78bcf777387d6a49ee7b8cd04fc97ad2cbe8b3
furuyan0906 <yanfuru96ongaku@gmail.com>
2024-10-03 11:52:58 +0900

    update docker installation script for Ubuntu 24.04

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -50,14 +50,14 @@ add_apt_repositories () {
     echo "*"
 
     # for docker
-    # ref: https://qiita.com/haveAbook/items/0d0ae20a19214f65e7cd
-    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
+    # ref: https://docs.docker.com/engine/install/ubuntu/
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove --purge $pkg; done
     sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
     echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # for Go-Lang
