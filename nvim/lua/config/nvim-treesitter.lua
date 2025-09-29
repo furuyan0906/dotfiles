a0d936d8983e6adf9b36dbfbf71831c6872a0958
furuyan0906 <yanfuru96ongaku@gmail.com>
2025-09-29 17:15:38 +0900

    minor changes for nvim's config

--- nvim/lua/config/nvim-treesitter.lua
+++ nvim/lua/config/nvim-treesitter.lua
@@ -2,15 +2,12 @@
 -- nvim-treesitter/nvim-treesitter の設定
 
 require('nvim-treesitter.configs').setup({
-    -- Automatically install missing parsers whne entering buffer --
+    -- Automatically install missing parsers when entering buffer --
     auto_install = true,
-
     -- List of parsers to ignore installing --
     ignore_install = { },
-
     highlight = {
         enable = true,
-
         -- List of language that will be disabled
         disable = { },
     },
--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,23 +1,22 @@
 ----------------------------------------------------------------------------------------------------------------------------
 -- nvim-telescope/telescope.nvim の設定
 
-local fb_actions = require('telescope').extensions.file_browser.actions
-
-require('telescope').setup({
+local telescope = require('telescope')
+telescope.load_extension('file_browser')
+telescope.setup({
     extension = {
         file_browser = {
             -- テーマにはivyを使用する --
-            theme = 'ivy', 
+            theme = 'ivy',
             -- Neovim標準エクスプローラのNetrwをDisableにしてtelescope-file-browserを代わりに使用する --
             hijack_netrw = true,
             mappings = {
                 -- ノーマルモード
                 ['n'] = {
-                    ['<C-h>'] = fb_actions.goto_home_dir,
+                    ['<C-h>'] = telescope.extensions.file_browser.actions.goto_home_dir,
                 },
             },
         },
     },
 })
-require('telescope').load_extension('file_browser')
 
